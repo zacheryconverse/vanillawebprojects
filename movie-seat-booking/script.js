852c771879659c233e644226b0a0b937a1d823ca
Zachery Converse <zacheryconverse@gmail.com>
2020-09-10 21:10:23 -0500

    Bug updateSelectedCount. Add starter JS

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -1,5 +1,30 @@
+const container = document.querySelector(".container");
+const seat = document.querySelectorAll(".row .seat:not(.occupied");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
 
+const ticketPrice = +movieSelect.value;
 
+const updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  container.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
 
 // const container = document.querySelector('.container');
 // const seats = document.querySelectorAll('.row .seat:not(.occupied)');
